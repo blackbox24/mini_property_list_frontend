5c3bee5549509074f738f96e3760e97a8e0173ad
blackbox24 <nicardid@gmail.com>
2025-05-11 22:48:10 +0000

    added main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+// import ResponsiveAppBar from './components/AppAppBar';
+import MainContent from './components/MainContent';
+// import Latest from './components/Latest';
+// import Footer from './components/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function Blog(props) {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline enableColorScheme />
+      {/* <ResponsiveAppBar /> */}
+      <Container
+        maxWidth="lg"
+        component="main"
+        sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
+      >
+        <MainContent />
+        {/* <Latest /> */}
+      </Container>
+      {/* <Footer /> */}
     </>
-  )
+  );
 }
-
-export default App
