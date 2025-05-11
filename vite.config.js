5a6ec33a52e516eae7d098c7bf952c26c65b40b4
blackbox24 <nicardid@gmail.com>
2025-05-11 20:06:22 +0000

    updated vite.conf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5500,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
 })
