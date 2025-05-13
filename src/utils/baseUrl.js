e6054dbd39e256f554e86cb148170c797f2c8614
blackbox24 <nicardid@gmail.com>
2025-05-13 15:02:49 +0000

    modified baseUrl

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://127.0.0.1:3000' : "https://simplepropertylistappbackend-production.up.railway.app";
+const baseUrl = "https://simplepropertylistappbackend-production.up.railway.app"; // process.env.NODE_ENV === 'production' ? 'https://127.0.0.1:3000' : 
 export default baseUrl;
